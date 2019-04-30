4807a159cb46ae92dd6d316e02588ffda312cba1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-30 05:34:48 +0000

    Deploy vivekkravindraa/travis-app to github.com/vivekkravindraa/travis-app.git:gh-pages

---
+++ precache-manifest.f41a13bd74a3bbcd0f61acf00732b91b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a431625031b1a5072b09f3cbabb24a",
+    "url": "/travis-app/index.html"
+  },
+  {
+    "revision": "81f4ec0504d83b4aba02",
+    "url": "/travis-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/travis-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "81f4ec0504d83b4aba02",
+    "url": "/travis-app/static/js/main.3d83a418.chunk.js"
+  },
+  {
+    "revision": "b2e1cbce52ee55d43fea",
+    "url": "/travis-app/static/js/runtime~main.cb8645c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
